76115cd1bbb5c1f0fa12cca6f01213f2285ce872
ESi <soobhhann@gmail.com>
2020-09-06 04:23:57 +0430

    about v2

--- src/components/summery/summery.js
+++ src/components/summery/summery.js
@@ -15,14 +15,7 @@ const Summery = () => {
           with SEO and optimizing websites. I did SEO for almost a year until I
           realized that I could build a website much easier than I thought, and
           yes, of course I first felt that the website I had was working. It
-          needs a series of facilities! So I started researching and learning
-          about the web, and after a month I was able to design my first
-          template that I had written with blank HTML and CSS. After a few
-          months, I started learning the beautiful language of JavaScript.After
-          a few courses and also doing a few mini-projects, I went to the React
-          js library, which was also full of wonders and cool things.Now I try
-          to get ahead of everyone by improving my work and create new things
-          that no one has done before
+          needs a series of facilities!
         </p>
         <Link to="/about">
           <Btn title="Read more" cls="btr" />
